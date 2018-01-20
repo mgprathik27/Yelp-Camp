33f2671eeb279fc1a6cde03a32d813fe0e7ab692
Prathik Ganiga <mgprathik27@gmail.com>
2018-01-20 09:14:10 -0600

    Show route added for campgrounds

--- app.js
+++ app.js
@@ -6,28 +6,26 @@ var bodyParser = require("body-parser");
 
 var userSchema = new mongoose.Schema({
 	name :String,
-	image:String
+	image:String,
+	Description : String,
 });
 
 var campmodel = mongoose.model("campmodel",userSchema);
-/*
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-];
-campgrounds.forEach(function(campground){
-campmodel.create({name: campground.name, image: campground.image},function(err,camp){
-	if(err){
-		console.log("Something went wrong");
-	} else {
+
+// var campgrounds = [
+//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg" , Description : "This is a huge camp"},
+//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg", Description : "This is a dusty camp"}
+// ];
+// campgrounds.forEach(function(campground){
+// campmodel.create({name: campground.name, image: campground.image, Description : campground.Description},function(err,camp){
+// 	if(err){
+// 		console.log("Something went wrong");
+// 	} else {
 		
-	}
-});	
-})
-*/
+// 	}
+// });	
+// })
+
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended : true}));
@@ -40,9 +38,19 @@ app.get("/campgrounds/new",function(req,res){
 	res.render("newCampground");
 })
 
+app.get("/campgrounds/:id",function(req,res){
+
+	campmodel.findById(req.params.id,function(err,campground){
+		if(!err){
+		res.render("show",{campground, campground});
+	}
+	})
+	
+})
+
 app.post("/campgrounds",function(req,res){
 
-	var campground = {name : req.body.name, image : req.body.image};
+	var campground = {name : req.body.name, image : req.body.image, Description : req.body.Description};
 	campmodel.create(campground,function(err,camp){
 	if(err){
 		console.log("Something went wrong");
@@ -58,7 +66,7 @@ app.get("/campgrounds",function(req,res){
 	if(err){
 		console.log("Something went wrong");
 	} else {
-			res.render("campground",{campgrounds: allcampgrounds});
+			res.render("index",{campgrounds: allcampgrounds});
 	}});
 
 })
